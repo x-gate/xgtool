2e1c37c4b57b68b518dd877f94d504ef601959ff
Vincent Chi <7449005+chivincent@users.noreply.github.com>
2023-10-17 23:10:39 +0800

    [Command] Convert Map to Tiled Map (#2)
    * refactor: dump_graphic with render for goroutine
    
    * refactor: move open resources into cmd/internal
    
    * feat: convert map to tiled map
    
    * docs: usage of converting map

--- cmd/dump_graphic.go
+++ cmd/dump_graphic.go
@@ -10,59 +10,14 @@ import (
 	"image/jpeg"
 	"os"
 	"sync"
+	"xgtool/cmd/internal"
 	"xgtool/pkg"
 )
 
-var bar *progressbar.ProgressBar
-var wg sync.WaitGroup
-var flags dumpGraphicFlags
-
-func main() {
-	if err := flags.Flags().Parse(os.Args[1:]); err != nil {
-		log.Err(err).Send()
-		return
-	}
-
-	log.Debug().Msgf("dumpGraphicFlags: %+v", flags)
-
-	files, err := openGraphicFiles(flags)
-	if err != nil {
-		log.Err(err).Send()
-		return
-	}
-	defer files.Close()
-
-	var palette pkg.Palette
-	if files.Palette != nil {
-		palette, err = pkg.NewPaletteFromCGP(files.Palette)
-		if err != nil {
-			log.Err(err).Send()
-			return
-		}
-	}
-
-	graphicIndex, _, err := pkg.MakeGraphicInfoIndexes(files.Info)
-	if err != nil {
-		log.Err(err).Send()
-		return
-	}
-
-	log.Debug().Msgf("len(graphicIndex): %d", len(graphicIndex))
-
-	bar = progressbar.Default(int64(len(graphicIndex)))
-
-	for _, gif := range graphicIndex {
-		if err = dumpGraphic(gif, files.Graphic, palette); err != nil {
-			log.Err(err).Send()
-			return
-		}
-		_ = bar.Add(1)
-	}
-
-	wg.Wait()
-
-	return
-}
+var (
+	bar *progressbar.ProgressBar
+	wg  sync.WaitGroup
+)
 
 type dumpGraphicFlags struct {
 	GraphicInfoFile string
@@ -81,34 +36,38 @@ func (f *dumpGraphicFlags) Flags() *flag.FlagSet {
 	return fs
 }
 
-type graphicFiles struct {
-	Info    *os.File
-	Graphic *os.File
-	Palette *os.File
-}
+var dgf dumpGraphicFlags
 
-func openGraphicFiles(flags dumpGraphicFlags) (files graphicFiles, err error) {
-	if files.Info, err = os.Open(flags.GraphicInfoFile); err != nil {
-		return files, err
-	}
-	if files.Graphic, err = os.Open(flags.GraphicFile); err != nil {
-		return files, err
-	}
-
-	// palette file is optional
-	if flags.PaletteFile == "" {
+func main() {
+	if err := dgf.Flags().Parse(os.Args[1:]); err != nil {
+		log.Err(err).Send()
 		return
 	}
 
-	files.Palette, err = os.Open(flags.PaletteFile)
+	log.Debug().Msgf("dumpGraphicFlags: %+v", dgf)
+
+	res, err := internal.OpenGraphicRes(dgf.GraphicInfoFile, dgf.GraphicFile, dgf.PaletteFile, "")
+	if err != nil {
+		log.Err(err).Send()
+		return
+	}
+	defer res.Close()
+
+	log.Debug().Msgf("len(graphicIndex): %d", len(res.IDIndex))
+
+	bar = progressbar.Default(int64(len(res.IDIndex)))
+
+	for _, gif := range res.IDIndex {
+		if err = dumpGraphic(gif, res.GraphicFile, res.Palette); err != nil {
+			log.Err(err).Send()
+			return
+		}
+		_ = bar.Add(1)
+	}
+
+	wg.Wait()
 
 	return
-}
-
-func (f graphicFiles) Close() {
-	_ = f.Info.Close()
-	_ = f.Graphic.Close()
-	_ = f.Palette.Close()
 }
 
 func dumpGraphic(info pkg.GraphicInfo, gf *os.File, palette pkg.Palette) error {
@@ -127,31 +86,39 @@ func dumpGraphic(info pkg.GraphicInfo, gf *os.File, palette pkg.Palette) error {
 		g.SetPalette(palette)
 	}
 
-	go func() {
-		wg.Add(1)
-		defer wg.Done()
-
-		var img image.Image
-		if img, err = g.ToImage(); err != nil {
-			log.Err(err).Send()
-			return
-		}
-
-		var out *os.File
-		if flags.DryRun {
-			out, err = os.OpenFile(os.DevNull, os.O_WRONLY, 0644)
-		} else {
-			out, err = os.OpenFile(fmt.Sprintf("output/%d.jpg", g.Info.ID), os.O_WRONLY|os.O_CREATE, 0644)
-		}
-		if err != nil {
-			log.Err(err).Send()
-		}
-		defer out.Close()
-
-		if err = jpeg.Encode(out, img, &jpeg.Options{Quality: 75}); err != nil {
-			log.Err(err).Send()
-		}
-	}()
+	go render(g)
 
 	return err
+}
+
+func render(g *pkg.Graphic) {
+	var err error
+
+	wg.Add(1)
+	defer wg.Done()
+
+	var img image.Image
+	if img, err = g.ToImage(); err != nil {
+		log.Err(err).Send()
+		return
+	}
+
+	var out *os.File
+	if out, err = output(*g.Info); err != nil {
+		log.Err(err).Send()
+		return
+	}
+	defer out.Close()
+
+	if err = jpeg.Encode(out, img, &jpeg.Options{Quality: 75}); err != nil {
+		log.Err(err).Send()
+	}
+}
+
+func output(gi pkg.GraphicInfo) (f *os.File, err error) {
+	if dgf.DryRun {
+		return os.OpenFile(os.DevNull, os.O_WRONLY, 0644)
+	}
+
+	return os.OpenFile(fmt.Sprintf("output/%d.jpg", gi.ID), os.O_WRONLY|os.O_CREATE, 0644)
 }
--- readme.md
+++ readme.md
@@ -6,14 +6,34 @@ The toolchain of x-gate, maybe works with CrossGate(?).
 
 ### Dump Graphic
 
+Dump graphics from `GraphicInfo.bin` and `Graphic.bin` to `./output` directory.
+
 ```shell
 $ export GRAPHIC_INFO_FILE="/Game/Crossgate/bin/GraphicInfo_66.bin" && \
   export GRAPHIC_FILE="/Game/Crossgate/bin/Graphic_66.bin" && \
-  export PALETTE_PATH="/Game/Crossgate/bin/pal/Palette" && \
+  export PALETTE_PATH="/Game/Crossgate/bin/pal/Palette"
 
-$ go run ./cmd/root.go dump-graphic \
+$ go run ./cmd/dump_graphic.go \
     -gif $GRAPHIC_INFO_FILE \
     -gf  $GRAPHIC_FILE \
     -pf  $PALETTE_PATH \
     -dry-run
+```
+
+### Convert Map
+
+Convert map into tmx (json) format.
+
+```shell
+$ export GRAPHIC_INFO_FILE="/Game/Crossgate/bin/GraphicInfo_66.bin" && \
+  export GRAPHIC_FILE="/Game/Crossgate/bin/Graphic_66.bin" && \
+  export PALETTE_PATH="/Game/Crossgate/bin/pal/Palette" && \
+  export MAP_FILE="/Game/Crossgate/map/1000.bin" # 法蘭城
+
+$ go run ./cmd/convert_map.go \
+    -gif $GRAPHIC_INFO_FILE \
+    -gf  $GRAPHIC_FILE \
+    -pf  $PALETTE_PATH \
+    -mf  $MAP_FILE \
+    -o output -n map.json
 ```
