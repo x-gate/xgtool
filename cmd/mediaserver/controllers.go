b48bacd8e9d0c6a3365f4a3313c87a533dafc4d9
Vincent Chi <song374561@chivincent.net>
2023-10-21 18:37:26 +0800

    feat: build http server with gin

---
+++ cmd/mediaserver/controllers.go
@@ -0,0 +1,7 @@
+package mediaserver
+
+import "github.com/gin-gonic/gin"
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "pong"})
+}
---
+++ cmd/mediaserver/root.go
@@ -0,0 +1,39 @@
+package mediaserver
+
+import (
+	"context"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+func MediaServer(ctx context.Context, args []string) error {
+	r := gin.Default()
+
+	r.GET("/ping", ping)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Send()
+		}
+	}()
+
+	// handle Ctrl+C signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	return nil
+}
